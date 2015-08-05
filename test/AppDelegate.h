52a01fd355508623877f3efc0dacfa393c1b849f
pooja-maha <pmaha@mobiquityinc.com>
2015-08-05 16:50:20 +0530

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Pooja Maha on 8/5/15.
+//  Copyright (c) 2015 Pooja Maha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
